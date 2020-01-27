920ed3cb2508f00a69efbb64bc33f10db15ab846
CodeIsaMystic <julienorjollet.wd@gmail.com>
2020-01-27 10:49:24 +0100

    lodash plugins sets & completed reveal-on-scroll

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,11 @@
 import '../styles/styles.css'
 import MobileMenu from './modules/MobileMenu'
+import RevealOnScroll from './modules/RevealOnScroll'
+
+new RevealOnScroll(document.querySelectorAll(".service-item"), 75)
+new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
+new RevealOnScroll(document.querySelectorAll(".btn--orange"), 50)
+
 
 let mobileMenu = new MobileMenu();
 
