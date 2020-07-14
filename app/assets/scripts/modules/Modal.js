11fad6612b9235cba0336d551143515c3638ef37
CodeIsaMystic <julienorjollet.wd@gmail.com>
2020-07-14 13:05:15 +0200

    add new content modal and set styles & mediaqueries

--- app/assets/scripts/modules/Modal.js
+++ app/assets/scripts/modules/Modal.js
@@ -32,20 +32,55 @@ class Modal {
   injectHTML() {
     document.body.insertAdjacentHTML('beforeend', `
     <div class="modal">
-    <div class="modal__inner">
-      <h2 class="section-title section-title--blue section-title--less-margin"><img src="assets/img/icons/mail.svg" class="section-title__icon section-title__icon--mail"> Get in <strong>Touch</strong></h2>
-      <div class="wrapper wrapper--narrow">
-        <p class="modal__description">We will have an online order system in place soon. Until then, connect with us on any of the platforms below!</p>
-      </div>
+      <div class="modal__inner">
 
-      <div class="social-icons">
-        <a href="#" class="social-icons__icon"><img src="assets/img/icons/facebook.svg" alt="Facebook"></a>
-        <a href="#" class="social-icons__icon"><img src="assets/img/icons/twitter.svg" alt="Twitter"></a>
-        <a href="#" class="social-icons__icon"><img src="assets/img/icons/instagram.svg" alt="Instagram"></a>
-        <a href="#" class="social-icons__icon"><img src="assets/img/icons/youtube.svg" alt="YouTube"></a>
+        <!-- Modal title -->
+        <h2 class="section-title section-title--blue section-title--less-margin">
+          <img src="assets/img/icons/mail.svg" class="section-title__icon"> 
+          Commences <strong>Dès Aujourd'hui</strong> !
+        </h2>
+
+        <div class="wrapper wrapper--narrow">
+
+          <!-- P description -->
+          <p class="modal__description d-none-until-XSmall">
+          Dès aujourd'hui, tu accèdes au premier <strong>questionnaire d'entrée de Coaching</strong>. C'est gratuit, aucun engagement, cela nous permet déjà de prendre les premières informations nécessaires pour te proposer la méthode adaptée à tes besoins et te conseiller selon tes besoins vers les priorités que tu pourrai mettre en place. 
+          </p>
+          <p class="modal__description ">
+          Il suffit de 2 minutes pour remplir ce formulaire et tu auras <strong>ton premier conseil coach sport santé entièrement gratuit</strong>. 
+          Après l'envoi, tu seras rediriger directement vers le questionnaire. 
+          </p>
+
+          <!-- Form section -->
+          <form id="formSendSurvey">
+  
+            <div class="form-group">
+              <label id="firstname" for="firstname">Prénom</label>
+              <input type="text" name="firstname" id="firstname" class="form-control" placeholder="Ecrivez votre prénom" required>
+            </div>
+  
+            <div class="form-group">
+              <label id="lastname" for="lastname">Nom</label>
+              <input type="text" name="lastname" id="lastname" class="form-control" placeholder="Ecrivez votre nom" required>
+            </div>
+  
+            <div class="form-group">
+              <label for="email" id="email-label">Email</label>
+              <input type="email" name="email" id="email" class="form-control" placeholder="Ecrivez votre email" required>
+            </div>
+  
+            <button type="submit" id="submit" class="btn-submit">
+              C'est Parti !
+            </button>
+  
+          </form>
+
+        </div>
+          
       </div>
-    </div>
-    <div class="modal__close">X</div>
+  <!--</div>-->
+
+  <div class="modal__close">X</div>
   </div>
     `);
   }
